4242938d9dabdbaa570662520d30ccbff89157b3
Unknown <joshcomley@googlemail.com>
2017-10-16 19:48:58 +0100

    NA

--- push.bat
+++ push.bat
@@ -1,1 +1,2 @@
-nuget push "%~dp0Packaged\*.nupkg" -Source https://www.myget.org/F/brandless/api/v2/package
+call del "%~dp0Packaged\Tun*.nupkg"
+call nuget push "%~dp0Packaged\*.nupkg" -Source https://www.myget.org/F/brandless/api/v2/package
