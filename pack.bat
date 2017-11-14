4a9947e03b29c9857c561ef598374bc56e5bf225
Josh Comley <joshcomley@googlemail.com>
2017-11-14 17:13:46 +0000

    Version bump

--- pack.bat
+++ pack.bat
@@ -1,2 +1,3 @@
 call del Packaged\* /Q
+call lprun "D:\Code\PowerZero\Code\Projects\DeveloperBox\LINQPad\Queries\Brandless Project Tools\Increment version.xml.linq" "%~dp0version.xml"
 call dotnet pack Code/ --output "%~dp0Packaged" --include-symbols --include-source -c Release
