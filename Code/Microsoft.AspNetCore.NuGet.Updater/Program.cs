4718ce8a0c95ed4a19593bb80288aa3b8588940e
Josh Comley <joshcomley@googlemail.com>
2018-05-05 15:52:57 +0200

    Added NuGet updater project

---
+++ Code/Microsoft.AspNetCore.NuGet.Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using NuGet.Updater.Core;
+
+namespace Microsoft.AspNetCore.NuGet.Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NuGetUpdater.UpdateAllFromFirstParentSolutionFolder(new[] { "https://www.myget.org/F/brandless/api/v2" });
+        }
+    }
+}
